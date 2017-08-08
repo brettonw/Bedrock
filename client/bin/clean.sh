fadb5702dfebb27f158ee7f15610c2f8ac2fc225
Bretton Wade <bretton.wade@gmail.com>
2017-08-08 18:12:53 -0400

    remove client (step 2)

--- client/bin/clean.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-. bin/common.sh
-
-echo "Cleaning $PROJECT_NAME in $PROJECT_DIR";
-
-rm -rf "$TARGET_DIR";
-echo "Removed 'target' as $TARGET_DIR";
--- client/bin/test.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-. bin/common.sh
-
-echo "Testing $PROJECT_NAME in $PROJECT_DIR";
-
-return 0;
