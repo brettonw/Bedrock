8878936e04d5cd7b97fd669d7984b85468dd4cea
Bretton Wade <bretton.wade@gmail.com>
2017-08-11 23:42:20 -0400

    versioned snapshots

--- applications/bedrock-site/bin/generate-sources.sh
+++ applications/bedrock-site/bin/generate-sources.sh
@@ -8,12 +8,17 @@ if [ ! -d "$TARGET_DIR" ]; then
     mkdir -p "$TARGET_DIR"
 fi
 
+# generate the version file
+VERSION_FILE="$TARGET_DIR/version.js";
+echo "Bedrock.version = \"$PROJECT_VERSION\";" > "$VERSION_FILE";
+
 # concatenate the files list into the target directory
 echo "Compile - concatenate source files";
 CONCAT="$TARGET_DIR/$PROJECT_NAME-concat.js";
 pushd "$SRC_DIR";
 cat                                 \
     bedrock.js                      \
+    $VERSION_FILE                   \
     log-level.js                    \
     base.js                         \
     utility.js                      \
@@ -30,6 +35,7 @@ popd;
 # preprocess the debug build out to the artifact
 echo "Preprocess debug build to $DEBUG_ARTIFACT";
 gcc -E -P -CC -xc++ -DDEBUG -o"$DEBUG_ARTIFACT" "$CONCAT"
+cp "$DEBUG_ARTIFACT" "$DEBUG_ARTIFACT_FINAL";
 
 # preprocess the release build
 PREPROCESS_RELEASE_TARGET="$TARGET_DIR/preprocess-$PROJECT_NAME-release.js";
@@ -39,5 +45,6 @@ gcc -E -P -CC -xc++ -o"$PREPROCESS_RELEASE_TARGET" "$CONCAT"
 # minify the release build
 echo "Minify release build to $RELEASE_ARTIFACT";
 uglifyjs "$PREPROCESS_RELEASE_TARGET" -o "$RELEASE_ARTIFACT"
+cp "$RELEASE_ARTIFACT" "$RELEASE_ARTIFACT_FINAL";
 
 echo "Done.";
