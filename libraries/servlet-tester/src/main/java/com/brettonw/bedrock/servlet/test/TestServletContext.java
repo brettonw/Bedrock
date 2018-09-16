dfac0dae90a30317ad39556a2d3070c3ce1ce183
Bretton Wade <bretton.wade@gmail.com>
2018-09-16 11:59:24 -0400

    I love/hate git - putting back my servlet tester changes for tomcat 9

--- libraries/servlet-tester/src/main/java/com/brettonw/bedrock/servlet/test/TestServletContext.java
+++ libraries/servlet-tester/src/main/java/com/brettonw/bedrock/servlet/test/TestServletContext.java
@@ -106,14 +106,17 @@ public class TestServletContext implements ServletContext {
 
     @Override
     public void log (String s) {
+
     }
 
     @Override
     public void log (Exception e, String s) {
+
     }
 
     @Override
     public void log (String s, Throwable throwable) {
+
     }
 
     @Override
@@ -248,6 +251,7 @@ public class TestServletContext implements ServletContext {
 
     @Override
     public void setSessionTrackingModes (Set<SessionTrackingMode> set) {
+
     }
 
     @Override
@@ -262,14 +266,17 @@ public class TestServletContext implements ServletContext {
 
     @Override
     public void addListener (String s) {
+
     }
 
     @Override
     public <T extends EventListener> void addListener (T t) {
+
     }
 
     @Override
     public void addListener (Class<? extends EventListener> aClass) {
+
     }
 
     @Override
@@ -289,10 +296,43 @@ public class TestServletContext implements ServletContext {
 
     @Override
     public void declareRoles (String... strings) {
+
     }
 
     @Override
     public String getVirtualServerName () {
         return null;
+    }
+
+    @Override
+    public ServletRegistration.Dynamic addJspFile (String s, String s1) {
+        return null;
+    }
+
+    @Override
+    public int getSessionTimeout () {
+        return 0;
+    }
+
+    @Override
+    public void setSessionTimeout (int i) {
+    }
+
+    @Override
+    public String getRequestCharacterEncoding () {
+        return StandardCharsets.UTF_8.name ();
+    }
+
+    @Override
+    public void setRequestCharacterEncoding (String s) {
+    }
+
+    @Override
+    public String getResponseCharacterEncoding () {
+        return StandardCharsets.UTF_8.name ();
+    }
+
+    @Override
+    public void setResponseCharacterEncoding (String s) {
     }
 }
