2127713505517392318c7ca57cdd28d954e9b9fa
Bretton Wade <brettonw.wade@gmail.com>
2020-05-01 15:40:49 -0400

    final updates for 1.4

--- libraries/base/src/main/java/com/brettonw/bedrock/Base.java
+++ libraries/base/src/main/java/com/brettonw/bedrock/Base.java
@@ -4,5 +4,6 @@ package com.brettonw.bedrock;
 public class Base {
     public static final int JDK_8 = 8;
     public static final int JDK_9 = 9;
-    public static final int JDK_TARGET = JDK_8;
+    public static final int JDK_11 = 11;
+    public static final int JDK_TARGET = JDK_11;
 }
