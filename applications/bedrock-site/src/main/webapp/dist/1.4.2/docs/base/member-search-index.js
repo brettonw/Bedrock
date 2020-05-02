b869313df70f8746f8fb1ab2182809c6602d93a3
Bretton Wade <brettonw.wade@gmail.com>
2020-05-02 19:25:03 -0400

    go git (1.4.2)

---
+++ applications/bedrock-site/src/main/webapp/dist/1.4.2/docs/base/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.brettonw.bedrock","c":"Base","l":"Base()","url":"%3Cinit%3E()"},{"p":"com.brettonw.bedrock","c":"Base","l":"JDK_11"},{"p":"com.brettonw.bedrock","c":"Base","l":"JDK_8"},{"p":"com.brettonw.bedrock","c":"Base","l":"JDK_9"},{"p":"com.brettonw.bedrock","c":"Base","l":"JDK_TARGET"}]
