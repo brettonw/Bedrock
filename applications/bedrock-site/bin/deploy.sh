b7943db7f683070ea7f38377f7c04472324a15fa
Bretton Wade <brettonw.wade@gmail.com>
2020-05-03 10:28:28 -0400

    update install and deploy with debugging info

--- applications/bedrock-site/bin/deploy.sh
+++ applications/bedrock-site/bin/deploy.sh
@@ -13,14 +13,20 @@ echo "Project: $PROJECT_NAME@v$PROJECT_VERSION";
 DOCKER_COUNT=$(docker-machine ls | grep default | wc -l | xargs);
 if [ "$DOCKER_COUNT" -eq "1" ]; then
   eval $(docker-machine env)
+  echo "Deploy: pushing docker tag $PROJECT_NAME:$PROJECT_VERSION from machine ($DOCKER_MACHINE_NAME) to AWS";
+
   # get a few user params (AWS_ACCOUNT_ID, AWS_REGION, AWS_PROFILE)
   . ~/.aws/bedrock.sh;
 
-  echo "Repository Name: $PROJECT_NAME";
   DOCKER_REPOSITORY=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME;
+  echo "Deploy: Repository = $DOCKER_REPOSITORY";
 
   aws --profile $AWS_PROFILE ecr describe-repositories --repository-names $PROJECT_NAME || aws --profile $AWS_PROFILE ecr create-repository --repository-name $PROJECT_NAME
   docker tag $PROJECT_NAME:$PROJECT_VERSION $DOCKER_REPOSITORY;
   aws --profile $AWS_PROFILE ecr get-login-password | docker login --username AWS --password-stdin $DOCKER_REPOSITORY;
   docker push $DOCKER_REPOSITORY;
+
+  echo "Deploy: Finished";
+else
+  echo "Deploy: FAILED to deploy from docker";
 fi
--- applications/bedrock-site/bin/install.sh
+++ applications/bedrock-site/bin/install.sh
@@ -16,10 +16,16 @@ TARGET_DIR="$PROJECT_DIR/target";
 DOCKER_COUNT=$(docker-machine ls | grep default | wc -l | xargs);
 if [ "$DOCKER_COUNT" -eq "1" ]; then
   eval $(docker-machine env)
+  echo "Install: building docker tag $PROJECT_NAME:$PROJECT_VERSION using machine ($DOCKER_MACHINE_NAME)";
 
   cp -r $PROJECT_DIR/src/main/docker $TARGET_DIR/docker
   cp $TARGET_DIR/bedrock-site.war $TARGET_DIR/docker/ROOT.war
   pushd $TARGET_DIR/docker
   docker build --tag $PROJECT_NAME:$PROJECT_VERSION .
   popd
+
+  echo "Install: Finished";
+
+else
+  echo "Install: FAILED to deploy from docker";
 fi
