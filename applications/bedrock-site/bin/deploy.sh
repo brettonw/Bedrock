424fad985f5cce577d266397371bb2a8c02a39aa
Bretton Wade <bretton.wade@gmail.com>
2021-12-10 14:49:40 -0500

    master - update log4j -and docker build steps

--- applications/bedrock-site/bin/deploy.sh
+++ applications/bedrock-site/bin/deploy.sh
@@ -3,40 +3,32 @@
 . bin/common.sh $1
 
 # exit on any error
-#set -e
 
 # docker setup
-DOCKER_COUNT=$(docker-machine ls | grep default | grep -i running | wc -l | xargs);
-if [ "$DOCKER_COUNT" -ne "1" ]; then
-  docker-machine start default;
-fi
+set -e
+DOCKER_COUNT=$(docker system info | grep -i images | awk '{print $2}');
+set +e
 
-DOCKER_COUNT=$(docker-machine ls | grep default | grep -i running | wc -l | xargs);
-if [ "$DOCKER_COUNT" -eq "1" ]; then
-  eval $(docker-machine env)
-  echo "Deploy: pushing docker tag $PROJECT_NAME:$PROJECT_VERSION from machine ($DOCKER_MACHINE_NAME) to AWS";
+echo "Deploy: pushing docker tag $PROJECT_NAME:$PROJECT_VERSION from machine ($DOCKER_MACHINE_NAME) to AWS";
 
-  # get a few user params (AWS_ACCOUNT_ID, AWS_REGION, AWS_PROFILE)
-  . ~/.aws/bedrock.sh;
+# get a few user params (AWS_ACCOUNT_ID, AWS_REGION, AWS_PROFILE)
+. ~/.aws/bedrock.sh;
 
-  DOCKER_REPOSITORY=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME;
-  echo "Deploy: Repository ($DOCKER_REPOSITORY)";
-  aws --profile $AWS_PROFILE ecr create-repository --repository-name $PROJECT_NAME || echo "Deploy: Repository for $PROJECT_NAME exists";
+DOCKER_REPOSITORY=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME;
+echo "Deploy: Repository ($DOCKER_REPOSITORY)";
+aws --profile $AWS_PROFILE ecr create-repository --repository-name $PROJECT_NAME || echo "Deploy: Repository for $PROJECT_NAME exists";
 
-  echo "Deploy: docker tag";
-  docker tag $PROJECT_NAME:$PROJECT_VERSION $DOCKER_REPOSITORY;
+echo "Deploy: docker tag";
+docker tag $PROJECT_NAME:$PROJECT_VERSION $DOCKER_REPOSITORY;
 
-  echo "Deploy: get login password";
-  LOGIN_PASSWORD=$(aws --profile $AWS_PROFILE ecr get-login-password);
-  #echo "Login password: $LOGIN_PASSWORD";
+echo "Deploy: get login password";
+LOGIN_PASSWORD=$(aws --profile $AWS_PROFILE ecr get-login-password);
+#echo "Login password: $LOGIN_PASSWORD";
 
-  echo "Deploy: set login";
-  echo $LOGIN_PASSWORD | docker login --username AWS --password-stdin $DOCKER_REPOSITORY;
+echo "Deploy: set login";
+echo $LOGIN_PASSWORD | docker login --username AWS --password-stdin $DOCKER_REPOSITORY;
 
-  echo "Deploy: push repository";
-  docker push $DOCKER_REPOSITORY;
+echo "Deploy: push repository";
+docker push $DOCKER_REPOSITORY;
 
-  echo "Deploy: Finished";
-else
-  echo "Deploy: FAILED to deploy from docker";
-fi
+echo "Deploy: Finished";
--- applications/bedrock-site/bin/install.sh
+++ applications/bedrock-site/bin/install.sh
@@ -9,23 +9,16 @@ set -e
 TARGET_DIR="$PROJECT_DIR/target";
 
 # docker setup
-DOCKER_COUNT=$(docker-machine ls | grep default | grep -i running | wc -l | xargs);
-if [ "$DOCKER_COUNT" -ne "1" ]; then
-  docker-machine start default;
-fi
+set -e
+DOCKER_COUNT=$(docker system info | grep -i images | awk '{print $2}');
+set +e
 
-DOCKER_COUNT=$(docker-machine ls | grep default | grep -i running | wc -l | xargs);
-if [ "$DOCKER_COUNT" -eq "1" ]; then
-  eval $(docker-machine env)
-  echo "Install: building docker tag $PROJECT_NAME:${PROJECT_VERSION,,} using machine ($DOCKER_MACHINE_NAME)";
+echo "Install: building docker tag $PROJECT_NAME:${PROJECT_VERSION,,} using machine ($DOCKER_MACHINE_NAME)";
 
-  cp -r $PROJECT_DIR/src/main/docker $TARGET_DIR/docker
-  cp $TARGET_DIR/bedrock.war $TARGET_DIR/docker/ROOT.war
-  pushd $TARGET_DIR/docker
-  docker build --tag "$PROJECT_NAME:${PROJECT_VERSION,,}" .
-  popd
+cp -r $PROJECT_DIR/src/main/docker $TARGET_DIR/docker
+cp $TARGET_DIR/bedrock.war $TARGET_DIR/docker/ROOT.war
+pushd $TARGET_DIR/docker
+docker build --tag "$PROJECT_NAME:${PROJECT_VERSION,,}" .
+popd
 
-  echo "Install: Finished";
-else
-  echo "Install: FAILED to deploy from docker";
-fi
+echo "Install: Finished";
